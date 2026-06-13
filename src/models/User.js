8b3cc36cbd53d0c86d7c60bede367eb6bdc09e18
Marisol Lamas <marilamas02@gmail.com>
2026-06-13 12:15:12 -0300

    feat: simplifica el middleware de hash de contraseña eliminando el uso de next()

--- src/models/User.js
+++ src/models/User.js
@@ -40,10 +40,9 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
   this.password = await bcrypt.hash(this.password, 12);
-  next();
 });
 
 userSchema.methods.compararPassword = function (candidato) {
