f087138532e75652b00ebb4b4f2a68f0c1d21f50
leandroblanca <ezepiolablank@gmail.com>
2026-06-15 16:42:20 -0300

    fix: cree una fucion para bloquer los turnos

--- src/models/Schedule.js
+++ src/models/Schedule.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const scheduleSchema = new mongoose.Schema({
     complexId:{
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'complex',
+        ref: 'complejo',
         require: true,
         unique: true
     },
