1aa0f4dde302bd1571c835f74a901104edd44fa9
leandroblanca <ezepiolablank@gmail.com>
2026-06-15 17:45:33 -0300

    fix: Termine la parte del bloque de turnos, hice el schema de mongo

--- src/models/Blockout.js
+++ src/models/Blockout.js
@@ -4,8 +4,28 @@ const blockoutSchema = new mongoose.Schema({
     complexId:{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'complejo',
-        requiere: true,
+        required: true,
         unique: true,
     },
-    
-})
+    courtId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'court',
+        default: null
+    },
+    name: { type: String, required: true},
+    recurrence : {
+        type: String,
+        enum: ['daily', 'weekly'],
+        required: true
+    },
+    dayOfWeek: {
+        type: String,
+        enum: ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'],
+        default: true
+    },
+    startTime:{type: String, required: true},
+    endTime:{type: String, required: true},
+    isActive:{type: Boolean, default:true},
+}, {timestamps: true });
+
+export default mongoose.model('Blockout', blockoutSchema);
