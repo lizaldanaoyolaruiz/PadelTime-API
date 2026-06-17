42e00bc4b2e5d506d92952934dd3b85439e23c5d
leandroblanca <ezepiolablank@gmail.com>
2026-06-17 13:25:13 -0300

    fix: estoy haciendo la carpeta de scheduleController lo que hice fue obtener la configuracion de horarios, bloqueosy metricas, tambien actualice las configuracios de los dias actios, que dia queda y horarios

--- src/models/Schedule.js
+++ src/models/Schedule.js
@@ -8,18 +8,18 @@ const scheduleSchema = new mongoose.Schema({
         unique: true
     },
 
-    onlineStatus:{Type: Boolean, default: true},
-    publicBookingEnabled: {Type: Boolean, degault: true},
+    onlineStatus:{Type: boolea, default: true},
+    publicBookingEnabled: {Type: boolea, default: true},
     openingDays: {
-        type: [String],
+        Type: [String],
         enum:['lunes','martes','miercoles','jueves','viernes','sabado','domingo'],
-        default: ['lunes', 'martes','miercoles', 'jueve', 'vierne']
+        default: ['lunes', 'martes','miercoles', 'jueves', 'viernes']
     },
     openTime: {type: String, default: '8 AM'},
     closeTime: {type: String, default: '11PM'},
     activeExtra1: {type: String, default: '11PM'},
     activeExtra2: {type: String, default: '11PM'},
-    update: {type: mongoose.Schema.Type.ObjectId, ref: 'user'}
+    update: {Type: mongoose.Schema.Type.ObjectId, ref: 'user'}
 },{timestamps: true});
 
 export default mongoose.model('Schedule', scheduleSchema);
