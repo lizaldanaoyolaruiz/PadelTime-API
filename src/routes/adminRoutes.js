133ad674ba42ce0071d5d7d13318e4891c488a29
Marisol Lamas <marilamas02@gmail.com>
2026-06-17 19:42:45 -0300

    refactor: migrar a módulos ES6 en modelos y rutas

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,18 +1,16 @@
-const express = require('express');
-const { body } = require('express-validator');
-const { getAdminUsers, approveAdmin, rejectAdmin } = require('../controllers/adminController');
-const { protect } = require('../middlewares/authMiddleware');
-const { requireRole } = require('../middlewares/roleMiddleware');
-const validate = require('../middlewares/validateMiddleware');
+import { Router } from 'express';
+import { body } from 'express-validator';
+import { getAdminUsers, approveAdmin, rejectAdmin } from '../controllers/adminController.js';
+import { protect } from '../middlewares/authMiddleware.js';
+import { requireRole } from '../middlewares/roleMiddleware.js';
+import validate from '../middlewares/validateMiddleware.js';
 
-const router = express.Router();
+const router = Router();
 
 router.use(protect, requireRole('superadmin'));
 
 router.get('/users', getAdminUsers);
 router.patch('/users/:id/approve', approveAdmin);
-router.patch('/users/:id/reject', [
-  body('reason').optional().trim(),
-], validate, rejectAdmin);
+router.patch('/users/:id/reject', [body('reason').optional().trim()], validate, rejectAdmin);
 
-module.exports = router;
+export default router;
