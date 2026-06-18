929ab35d2c995264e2668aac06902dee9b76a72e
facu-jc <camanof94@gmail.com>
2026-06-18 05:31:29 -0300

    chore: comenta la ruta de reports en el enrutador principal

--- src/routes/index.js
+++ src/routes/index.js
@@ -19,7 +19,7 @@ router.use('/bookings', bookingRoutes);
 router.use('/admin', adminRoutes);
 router.use('/chatbot', chatbotRoutes);
 router.use('/reviews', reviewRoutes);
-router.use('/reports', reportRoutes);
+//router.use('/reports', reportRoutes);
 router.use("/metrics", metricsRoutes);
 router.use('/payments', paymentRoutes);
 
