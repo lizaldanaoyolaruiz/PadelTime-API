bde7926372fc7e856d2235b9660553baedcd56c2
Octavio <octaviofernandez40999@gmail.com>
2026-06-17 23:57:03 -0300

    feat: refactoriza index.js para usar importaciones ES6 y elimina código innecesario

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,27 +7,9 @@ import adminRoutes from './adminRoutes.js';
 import chatbotRoutes from './chatbotRoutes.js';
 import reviewRoutes from './reviewRoutes.js';
 import reportRoutes from './reportRoutes.js';
+import paymentRoutes from './paymentRoutes.js';
 
-<<<<<<< HEAD
-const authRoutes = require('./authRoutes');
-const complexRoutes = require('./complexRoutes');
-const courtRoutes = require('./courtRoutes');
-const bookingRoutes = require('./bookingRoutes');
-const adminRoutes = require('./adminRoutes');
-const chatbotRoutes = require('./chatbotRoutes');
-const reviewRoutes = require('./reviewRoutes');
-const clubRoutes = require('./clubRoutes');
-const superAdminGestionRoutes = require('./superAdminGestionRoutes');
-const reportRoutes = require('./reportRoutes');
-const paymentRoutes = require('./paymentRoutes');
-const router = express.Router();
-
-router.get('/health', (req, res) => {
-  res.json({ status: 'PadelTime API running' });
-});
-=======
 const router = Router();
->>>>>>> 900dad729b7d2cfd6a908852d27b185064b89f15
 
 router.get('/health', (req, res) => res.json({ status: 'PadelTime API running' }));
 router.use('/auth', authRoutes);
