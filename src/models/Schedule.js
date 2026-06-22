0c7de3c701831cfc2c449e8a97a0cf7bc003fc3c
leandroblanca <ezepiolablank@gmail.com>
2026-06-22 11:55:36 -0300

    fix: corrige tipos de datos en el esquema de horarios

--- src/models/Schedule.js
+++ src/models/Schedule.js
@@ -15,7 +15,10 @@ const scheduleSchema = new mongoose.Schema({
         enum:['lunes','martes','miercoles','jueves','viernes','sabado','domingo'],
         default: ['lunes', 'martes','miercoles', 'jueves', 'viernes']
     },
-  
+    openTime: {type: String, default: '8 AM'},
+    closeTime: {type: String, default: '11PM'},
+    activeExtra1: {type: String, default: '11PM'},
+    activeExtra2: {type: String, default: '11PM'},
     update: {Type: mongoose.Schema.Type.ObjectId, ref: 'user'}
 },{timestamps: true});
 
